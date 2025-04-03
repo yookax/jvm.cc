aff84b14165bd6d92d1032d691508e53875ba874
yookax <yookax@outlook.com>
2025-04-03 16:29:15 +0800

    .

--- src/native/java/lang/Double.cpp
+++ src/native/java/lang/Double.cpp
@@ -3,27 +3,41 @@
 
 module native;
 
+import std.core;
 import slot;
 import runtime;
+import convert;
 
 // Find the bit pattern corresponding to a given float, NOT collapsing NaNs
 // public static native long doubleToRawLongBits(double value);
 void doubleToRawLongBits(Frame *f) {
-    unimplemented
+    slot_t *args = f->lvars;
+    auto value = slot::get<jdouble>(args);
+
+    uint8_t bytes[sizeof(value)];
+    double_to_bytes(value, bytes, std::endian::native);
+    jlong x = bytes_to_int64(bytes, std::endian::native);
+    f->pushl(x);
 }
 
 // Find the double float corresponding to a given bit pattern
 // public static native double longBitsToDouble(long bits);
 void longBitsToDouble(Frame *f) {
-    unimplemented
+    slot_t *args = f->lvars;
+    auto bits = slot::get<jlong>(args);
+
+    uint8_t bytes[sizeof(bits)];
+    int64_to_bytes(bits, bytes, std::endian::native);
+    jdouble x = bytes_to_double(bytes, std::endian::native);
+    f->pushd(x);
 }
 
 void java_lang_Double_registerNatives() {
 #undef R
 #define R(method, method_descriptor) \
-    registry("java/lang/double", #method, method_descriptor, method)
+    registry("java/lang/Double", #method, method_descriptor, method)
 
-//    R(doubleToRawLongBits, "(D)J");
-//    R(longBitsToDouble, "(J)D");
+    R(doubleToRawLongBits, "(D)J");
+    R(longBitsToDouble, "(J)D");
 }
 
